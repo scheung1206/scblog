2778c7332dd74c7d4a8d952b71336fd1aa9c8809
Stephen Cheung <stephencheung@Stephens-MacBook-Pro.local>
2016-06-06 13:14:33 -0700

    contact

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   get 'welcome/homepage'
   get 'welcome/about'
+  get 'welcome/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
