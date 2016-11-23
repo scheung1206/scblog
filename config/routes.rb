8d95a6b9275656d71ee751472baa959da250ec4d
Stephen Cheung <stephencheung@Stephens-MacBook-Pro.local>
2016-11-22 17:38:39 -0800

    Projects added

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'welcome/homepage'
   get 'welcome/about'
   get 'welcome/contact'
+  get 'welcome/project'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
